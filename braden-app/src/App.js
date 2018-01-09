2e24435ce5c9f0b70c174d7a523a052bdba509b7
Braden Casperson <bcasper@l-sff161gtf1-m.gap.com>
2018-01-09 09:23:54 -0800

    Added Google Analytics back in.

--- braden-app/src/App.js
+++ braden-app/src/App.js
@@ -15,13 +15,14 @@ class App extends Component {
           <div className="App-text-container">
             <h1 className="App-title">Braden Casperson</h1>
             <p className="App-intro">
-              I'm an engineer who does iOS work @GapInc. Check out my blog below. Feel free to contact me.
+              I'm an engineer who does iOS work @GapInc. Check out my blog on Medium below. Feel free to contact me.
             </p>
             <p className="App-links">
               <a href="https://www.linkedin.com/in/bradencasperson/" target="_blank"><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
               <a href="https://github.com/casperson" target="_blank"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
               <a href="https://twitter.com/bradencasperson" target="_blank"><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
               <a href="https://medium.com/@casperson" target="_blank"><i className="fa fa-medium fa-2x" aria-hidden="true"></i></a>
+              <a href="https://open.spotify.com/user/1246323348" target="_blank"><i className="fa fa-spotify fa-2x" aria-hidden="true"></i></a>
             </p>
           </div>
         </div>
